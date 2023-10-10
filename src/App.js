8cfe7fb57a3d02dd5ed2eee837902a9622751e6f
shivasai <shivasaigundoji@gmail.com>
2023-10-10 11:43:40 +0530

    commiting the modified files i.e App.js , index,html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+
+
 
 function App() {
   return (
-    <div>
-        Homepage
-    </div>
+    <>
+      <Header />
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
